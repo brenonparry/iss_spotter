2f4df3e77baab23fa2364499b0da11a55de0385d
Brenon Parry <brenonparry@gmail.com>
2021-12-03 05:28:46 +0000

    created fetchcoordsByIP function that requests a lat/long object

--- iss.js
+++ iss.js
@@ -9,13 +9,34 @@ const fetchMyIP = (callback) => {
         const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
         return callback(Error(msg), null);
       } else {
-        return callback(null, IP);
+        return callback(null, IP.ip);
       }
       
     } else {
       return callback(error, null);
     }
 
+  });
+};
+
+const fetchCoordsByIP = (IP, callback) => {
+  request(`https://freegeoip.app/json/${IP}`, (error, response, coords) => {
+    if (!error)  { // !error = good! It works!
+      const latLong = JSON.parse(coords);
+      const latitude = latLong.latitude;
+      const longitude = latLong.longitude;
+      const finalCoords = {latitude: latitude, longitude: longitude};
+
+      if (response.statusCode !== 200) {
+        const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${coords}`;
+        return callback(Error(msg), null);
+      } else {
+        return callback(null, finalCoords);
+      }
+      
+    } else {
+      return callback(error, null);
+    }
   });
 };
 
@@ -26,9 +47,4 @@ const fetchMyIP = (callback) => {
 
 
 
-
-
-
-
-
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIP };
