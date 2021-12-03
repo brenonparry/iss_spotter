0d1ea69b374358cffff8105c5df7dfb3ec4282db
Brenon Parry <brenonparry@gmail.com>
2021-12-03 17:56:43 +0000

    Updated indexto call fetchISSFlyOverTimes function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -16,4 +16,16 @@ fetchCoordsByIP('174.7.85.204', (error, ip) => {
   }
 
   console.log('It worked! Returned latitude/longittude: ' , ip);
+});
+
+const exampleCoords = { latitude: '49.3678', longitude: '-122.9278' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+
 });
