8111917917e2c58d9d2fdfdacde9407ccf6d8f59
Brenon Parry <brenonparry@gmail.com>
2021-12-03 05:29:35 +0000

    updated index to account for new fetchCoordsByIP function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -6,5 +6,14 @@ fetchMyIP((error, ip) => {
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned IP: ' , ip);
+});
+
+fetchCoordsByIP('174.7.85.204', (error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned latitude/longittude: ' , ip);
 });
